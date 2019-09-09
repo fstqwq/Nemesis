7bbb4f0808b081fd5d108ffaf59be3945ff8f818
fstqwq <fstqwq@foxmail.com>
2019-09-09 13:10:02 +0800

    fix update

--- src/Math/直线下格点统计.cpp
+++
@@ -1,6 +0,0 @@
-// $\sum_{i=0}^{n-1} \lfloor \frac{a+bi}{m}\rfloor$, $n,m,a,b>0$
-ll solve(ll n, ll a, ll b, ll m){
-	if (b == 0) return n * (a / m);
-	if (a >= m) return n * (a / m) + solve(n, a % m, b, m);
-	if (b >= m) return (n-1)*n/2*(b/m) + solve(n,a,b%m,m);
-	return solve((a + b * n) / m, (a + b * n) % m, m, b); }
