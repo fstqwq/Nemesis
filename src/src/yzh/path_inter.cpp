c5ab8f22707c03b9085781fc1abfb5a053166d17
fstqwq <fstqwq@foxmail.com>
2021-10-28 02:30:44 +0800

    modify some formats and tree path intersection

--- src/src/yzh/path_inter.cpp
+++ src/src/yzh/path_inter.cpp
@@ -1,10 +1,11 @@
 bool cmp(int a,int b){return dep[a]<dep[b];}
-path merge(path o1,path o2){
+path merge(path u, path v){
 	static int d[4], c[2];
-    if(!o1.x||!o2.x)return path({0,0});
-    d[0]=lca(o1.x,o2.x);d[1]=lca(o1.x,o2.y);d[2]=lca(o1.y,o2.x);d[3]=lca(o1.y,o2.y);
-    c[0]=lca(o1.x,o1.y);c[1]=lca(o2.x,o2.y);
-    sort(d,d+4,cmp); sort(c, c + 1, cmp);
-    if(dep[c[0]]<=dep[d[0]]&&dep[c[1]]<=dep[d[2]])
-	return path({d[2],d[3]});else return path({0, 0});//
-}    
+	if (!u.x||!v.x) return path(0, 0);
+	d[0]=lca(u.x,v.x); d[1]=lca(u.x,v.y);
+	d[2]=lca(u.y,v.x); d[3]=lca(u.y,v.y);
+	c[0]=lca(u.x,u.y); c[1]=lca(v.x,v.y);
+	sort(d,d+4,cmp); sort(c,c+1,cmp);
+	if (dep[c[0]] <= dep[d[0]] && dep[c[1]] <= dep[d[2]])
+		return path(d[2],d[3]);
+	else return path(0, 0); }
