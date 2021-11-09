982db0e85cee2be47882ba20048aaf49335c70f9
fstqwq <fstqwq@foxmail.com>
2021-11-10 02:24:22 +0800

    fix llfpm and radix sort; swap last two sections

--- src/src/Miscellany/LLFPM.cpp
+++ src/src/Miscellany/LLFPM.cpp
@@ -1,4 +1,10 @@
-LL mul(LL a, LL b, LL mod) {
-	LL ret = (a * b - LL((LD)a / mod * b) * mod) % mod;
-	return ret < 0 ? ret + mod : ret;
-}
+// kactl,  $M \le 7.2\cdot 10^{18}$
+ULL modmul(ULL a, ULL b, LL M) {
+	LL ret = a * b - M * ULL(1.L / M * a * b);
+	return ret + M * (ret < 0) - M * (ret >= (LL)M); }
+// orz@CF, M in 63 bit
+ULL modmul(ULL a, ULL b, LL M) {
+	ULL c = (long double)a * b / M;
+	LL ret = LL(x * y - c * M) % M;
+	return ret < 0 ? ret + M : ret;}
+// use int128 instead if c > 63 bit
--- src/src/Miscellany/RadixSort.cpp
+++ src/src/Miscellany/RadixSort.cpp
@@ -1,10 +1,11 @@
-void SORT(int a[], int c[], int n, int l) {
-	for (int i = 0; i < SZ; i++) b[i] = 0;
-	for (int i = 1; i <= n; i++) b[(a[i] >> l) & (SZ - 1)]++;
-	for (int i = 1; i < SZ; i++) b[i] += b[i - 1];
-	for (int i = n; i; i--) c[b[(a[i] >> l) & (SZ - 1)]--] = a[i]; }
-void Sort(int *a, int b){
-	if (b < 23333) sort(a + 1, a + b + 1);
-	else {
-		SORT(a, c, b, 0);
-		SORT(c, a, b, 14); } }
+const int SZ = 1 << 8; // almost always fit in L1 cache
+void SORT(int a[], int c[], int n, int w) {
+	for(int i=0; i<SZ; i++) b[i] = 0;
+	for(int i=1; i<=n; i++) b[(a[i]>>w) & (SZ-1)]++;
+	for(int i=1; i<SZ; i++) b[i] += b[i - 1];
+	for(int i=n; i; i--) c[b[(a[i]>>w) & (SZ-1)]--] = a[i];}
+void Sort(int *a, int n){
+	SORT(a, c, n, 0);
+	SORT(c, a, n, 8);
+	SORT(a, c, n, 16);
+	SORT(c, a, n, 24); }
