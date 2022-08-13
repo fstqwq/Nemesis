1b2aebaa7374ddb713108cbca267424bc7abc1d1
fstqwq <fstqwq@foxmail.com>
2022-08-14 04:29:49 +0800

    add omega and d to constant table

--- src/src/Miscellany/randomprimes.py
+++ src/src/Miscellany/randomprimes.py
@@ -1,36 +1,39 @@
 def isprime(x):
 	if x <= 3:
 		return x >= 2
-	for i in range(2, x):
+	if x % 2 == 0:
+		return False
+	for i in range(3, x, 2):
 		if x % i == 0:
 			return False
 		if i * i > x:
 			break
 	return True
 
-import random
-T = 10
-Range = [1e3, 1e5, 5e5, 1e6, 2e6, 1e7, 1e9]
-print("{")
-print("\\begin{itemize}")
-print(r"\setlength{\parindent}{0em}\setlength{\parskip}{0em}\renewcommand{\baselinestretch}{.5}")
+import random,datetime,time
+T = 7
+Range = [1e3, 1e5, 5e5, 1e6, 2e6, 1e7, 2e7, 1e9]
+print("{\\textbf{Random primes} generated at ", time.asctime( time.localtime(time.time()) ), "\\\\")
 for lim in Range:
-	print(("\\item[%0.0e] " % lim).replace("+0", ""), end="")
-	lower = int(lim * 0.96)
-	upper = int(lim * 1.06)
+	print(("\\textbf{%0.0e} " % lim).replace("+0", ""), end="")
+	lower = int(lim * 0.94)
+	upper = int(lim * 1.07)
 	cur = set ()
 	dead = 0
-	while len(cur) < T:
+	s = ""
+	while True:
 		x = random.randint(lower, upper)
 		if isprime(x) and x not in cur:
+			s += " " + str(x)
+			if len(s) > 62:
+				break
 			cur.add(x)
 		else:
 			dead += 1
-			if dead > 10000:
+			if dead > 500:
 				break
 	cur = sorted(cur)
 	for i in cur:
 		print(i, end=" ")
-	print()
-print("\\end{itemize}")
+	print("\\\\")
 print("}")
