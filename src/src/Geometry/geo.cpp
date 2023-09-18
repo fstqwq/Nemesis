fc3b824f9adecf89c26ff0a8c5364faf76e4dd5f
fstqwq <fstqwq@foxmail.com>
2023-09-18 16:33:16 +0800

    add min dis to convex hull, add triangulation

--- src/src/Geometry/geo.cpp
+++ src/src/Geometry/geo.cpp
@@ -1,17 +1,28 @@
 struct point {
 	point rot(double t) const { // 逆时针
 		return {x*cos(t) - y*sin(t), x*sin(t) + y*cos(t)};}
-	point rot90() const { return {-y, x}; } 
-	double len2() const { return x * x + y * y;}
-	double len() const { return sqrt(x * x + y * y);}
-	point unit() const {
-		double d = len(); return {x / d, y / d};}};
-bool point_on_segment(cp a,cl b){ // 点在线段上
-	return sgn (det(a - b.s, b.t - b.s)) == 0 // 在直线上
-	&& sgn (dot (b.s - a, b.t - a)) <= 0;}
+	point rot90() const { return {-y, x}; }};
 bool two_side(cp a,cp b,cl c) {
 	return sgn (det(a - c.s, c.t - c.s))
 		* sgn (det(b - c.s, c.t - c.s)) < 0; }
+point line_inter(cl a, cl b) { // 直线交点
+	double s1 = det(a.t - a.s, b.s - a.s);
+	double s2 = det(a.t - a.s, b.t - a.s);
+	return (b.s * s2 - b.t * s1) / (s2 - s1); }
+// 线切凸包，可直接实现半平面交
+vector <point> cut (const vector<point> &c, line p) {
+	vector <point> ret;
+	int n = (int) c.size();
+	if (!n) return ret;
+	for (int i = 0; i < n; ++i) {
+		int j = (i + 1) % n;
+		if (turn_left (p.s, p.t, c[i])) ret.push_back (c[i]);
+		if (two_side (c[i], c[j], p))
+			ret.push_back (line_inter (p, {c[i], c[j]})); }
+	return ret; }
+bool point_on_segment(cp a,cl b){ // 点在线段上
+	return sgn (det(a - b.s, b.t - b.s)) == 0 // 在直线上
+	&& sgn (dot (b.s - a, b.t - a)) <= 0;}
 bool intersect_judge(cl a,cl b) { // 线段判非严格交
 	if (point_on_segment (b.s, a)
 	|| point_on_segment (b.t, a)) return true;
@@ -19,10 +30,26 @@ bool intersect_judge(cl a,cl b) { // 线段判非严格交
 	|| point_on_segment (a.t, b)) return true;
 	return two_side (a.s, a.t, b)
 		&& two_side (b.s, b.t, a); }
-point line_intersect(cl a, cl b) { // 直线交点
-	double s1 = det(a.t - a.s, b.s - a.s);
-	double s2 = det(a.t - a.s, b.t - a.s);
-	return (b.s * s2 - b.t * s1) / (s2 - s1); }
+double point_to_line (cp a, cl b) { // 点到直线距离
+	return abs (det(b.t-b.s, a-b.s)) / dis (b.s, b.t); }
+point project_to_line (cp a, cl b) { // 点在直线投影
+	return b.s + (b.t - b.s)
+	* (dot (a - b.s, b.t - b.s) / dis2(b.t, b.s)); }
+double point_to_segment (cp a, cl b) { // 点到线段距离
+	if (sgn (dot (b.s - a, b.t - b.s))
+	* sgn (dot (b.t - a, b.t - b.s)) <= 0)
+		return abs(det(b.t - b.s, a - b.s)) / dis(b.s, b.t);
+	return min (dis (a, b.s), dis (a, b.t)); }
+bool in_polygon (cp p, const vector <point> & po) {
+	int n = (int) po.size (); int cnt = 0;
+	for (int i = 0; i < n; ++i) {
+		point a = po[i], b = po[(i + 1) % n];
+		if (point_on_segment (p, {a, b})) return true;
+		int x = sgn (det(p - a, b - a)),
+			y = sgn (a.y - p.y), z = sgn (b.y - p.y);
+		if (x > 0 && y <= 0 && z > 0) ++cnt;
+		if (x < 0 && z <= 0 && y > 0) --cnt; }
+	return cnt != 0; }
 bool point_on_ray (cp a, cl b) { // 点在射线上
 	return sgn (det(a - b.s, b.t - b.s)) == 0
 	&& sgn (dot (a - b.s, b.t - b.s)) >= 0; }
@@ -38,26 +65,6 @@ bool ray_intersect_judge(line a, line b) { // 射线判交
 	s1 = det(a.t - a.s, b.s - a.s);
 	s2 = det(a.t - a.s, b.t - a.s);
 	return sgn(s1) != sgn(s2 - s1); }
-double point_to_line (cp a, cl b) { // 点到直线距离
-	return abs (det(b.t-b.s, a-b.s)) / dis (b.s, b.t); }
-point project_to_line (cp a, cl b) { // 点在直线投影
-	return b.s + (b.t - b.s)
-	* (dot (a - b.s, b.t - b.s) / (b.t - b.s).len2 ()); }
-double point_to_segment (cp a, cl b) { // 点到线段距离
-	if (sgn (dot (b.s - a, b.t - b.s))
-	* sgn (dot (b.t - a, b.t - b.s)) <= 0)
-		return abs(det(b.t - b.s, a - b.s)) / dis(b.s, b.t);
-	return min (dis (a, b.s), dis (a, b.t)); }
-bool in_polygon (cp p, const vector <point> & po) {
-	int n = (int) po.size (); int cnt = 0;
-	for (int i = 0; i < n; ++i) {
-		point a = po[i], b = po[(i + 1) % n];
-		if (point_on_segment (p, line (a, b))) return true;
-		int x = sgn (det(p - a, b - a)),
-			y = sgn (a.y - p.y), z = sgn (b.y - p.y);
-		if (x > 0 && y <= 0 && z > 0) ++cnt;
-		if (x < 0 && z <= 0 && y > 0) --cnt; }
-	return cnt != 0; }
 vector <point> line_circle_intersect (cl a, cc b) {
 	if (sgn (point_to_line (b.c, a) - b.r) > 0)
 		return vector <point> ();
@@ -97,31 +104,22 @@ vector <line> extangent (cc a, cc b) {
 	if (sgn (a.r - b.r) == 0) {
 		point dir = b.c - a.c;
 		dir = (dir * a.r / dir.len ()).rot90 ();
-		ret.push_back (line (a.c + dir, b.c + dir));
-		ret.push_back (line (a.c - dir, b.c - dir));
+		ret.push_back ({a.c + dir, b.c + dir});
+		ret.push_back ({a.c - dir, b.c - dir});
 	} else {
 		point p = (b.c * a.r - a.c * b.r) / (a.r - b.r); 
 		vector pp = tangent (p, a), qq = tangent (p, b);
 		if (pp.size () == 2 && qq.size () == 2) {
 			if (sgn (a.r-b.r) < 0)
 				swap (pp[0], pp[1]), swap (qq[0], qq[1]);
-			ret.push_back(line (pp[0], qq[0]));
-			ret.push_back(line (pp[1], qq[1])); } }
+			ret.push_back({pp[0], qq[0]});
+			ret.push_back({pp[1], qq[1]}); } }
 	return ret; }
 vector <line> intangent (cc a, cc b) {
 	vector <line> ret;
 	point p = (b.c * a.r + a.c * b.r) / (a.r + b.r); 
 	vector pp = tangent (p, a), qq = tangent (p, b);
 	if (pp.size () == 2 && qq.size () == 2) {
-		ret.push_back (line (pp[0], qq[0]));
-		ret.push_back (line (pp[1], qq[1])); }
-	return ret; }
-vector <point> cut (const vector<point> &c, line p) {
-	vector <point> ret;
-	if (c.empty ()) return ret;
-	for (int i = 0; i < (int) c.size (); ++i) {
-		int j = (i + 1) % (int) c.size ();
-		if (turn_left (p.s, p.t, c[i])) ret.push_back (c[i]);
-		if (two_side (c[i], c[j], p))
-			ret.push_back (line_intersect (p, line (c[i], c[j]))); }
+		ret.push_back ({pp[0], qq[0]});
+		ret.push_back ({pp[1], qq[1]}); }
 	return ret; }
--- src/src/Geometry/logconvexhull.cpp
+++ src/src/Geometry/logconvexhull.cpp
@@ -1,23 +1,22 @@
 /* 给定凸包, $\log n$ 内完成各种询问, 具体操作有 :
-1. 判定一个点是否在凸包内
-2. 询问凸包外的点到凸包的两个切点
-3. 询问一个向量关于凸包的切点
-4. 询问一条直线和凸包的交点
-INF 为坐标范围, 需要定义点类 operator < > 为 pair(x, y)
-改成实数只需修改 sgn 函数, 以及把 LL 改为 double 即可
-传入凸包要求无重点, 面积非空, pair(x,y) 最小点放在第一个 */
-const int INF = 1e9;
+1. 点在凸包内 2. 凸包外的点到凸包的两个切点
+3. 向量关于凸包的切点 4. 直线和凸包的交点
+5. 凸包外的点到凸包距离
+INF 开到值域, point operator < > 为 pair(x, y)
+除 5 操作可改为整数: LD to LL
+传入凸包要求无重点, 面积非空 */
 struct Convex {
- int n;
+ int n, lz, uz;
  vector<point> a, upper, lower;
  Convex(vector<point> _a) : a(_a) {
- 	n = a.size(); int k = 0;
+ 	n = (int) a.size(); int k = 0;
  	for(int i = 1; i < n; ++ i) if (a[k] < a[i]) k = i;
  	for(int i = 0; i <= k; ++ i) lower.push_back(a[i]);
  	for(int i = k; i < n; ++ i) upper.push_back(a[i]);
  	upper.push_back(a[0]);
+ 	lz = (int) lower.size(); uz = (int) upper.size();
  }
- pair <LL, int> get_tan(vector <point> & con, point vec) {
+ pair <LD, int> get_tan(vector <point> & con, point vec) {
  	int l = 0, r = (int) con.size() - 2;
  	for ( ; l + 1 < r; ) {
  		int mid = (l + r) / 2;
@@ -42,24 +41,14 @@ struct Convex {
  	}
  	upd_tan(p, r % n, i0, i1);
  }
- int search(point u, point v, int l, int r) {
- 	int sl = sgn(det(v - u, a[l % n] - u));
- 	for ( ; l + 1 < r; ) {
- 		int mid = (l + r) / 2;
- 		int smid = sgn(det(v - u, a[mid % n] - u));
- 		if (smid == sl) l = mid;
- 		else r = mid;
- 	}
- 	return l % n;
- }
  // 判定点是否在凸包内, 在边界返回 true
  bool contain(point p) {
  	if (p.x < lower[0].x || p.x > lower.back().x) return false;
- 	int id = lower_bound(lower.begin(), lower.end(), point(p.x, -INF)) - lower.begin();
+ 	int id = int(lower_bound(lower.begin(), lower.end(), point(p.x, -INF)) - lower.begin());
  	if (lower[id].x == p.x) {
  		if (lower[id].y > p.y) return false;
  	} else if (det(lower[id - 1] - p, lower[id] - p) < 0) return false;
- 	id = lower_bound(upper.begin(), upper.end(), point(p.x, INF), greater<point>()) - upper.begin();
+ 	id = int(lower_bound(upper.begin(), upper.end(), point(p.x, INF), greater<point>()) - upper.begin());
  	if (upper[id].x == p.x) {
  		if (upper[id].y < p.y) return false;
  	} else if (det(upper[id - 1] - p, upper[id] - p) < 0) return false;
@@ -70,26 +59,54 @@ struct Convex {
  	i0 = i1 = 0;
  	int id = int(lower_bound(lower.begin(), lower.end(), p) - lower.begin());
  	search(0, id, p, i0, i1);
- 	search(id, (int)lower.size(), p, i0, i1);
+ 	search(id, lz, p, i0, i1);
  	id = int(lower_bound(upper.begin(), upper.end(), p, greater <point> ()) - upper.begin());
- 	search((int)lower.size() - 1, (int) lower.size() - 1 + id, p, i0, i1);
- 	search((int)lower.size() - 1 + id, (int) lower.size() - 1 + (int)upper.size(), p, i0, i1);
+ 	search(lz - 1, lz - 1 + id, p, i0, i1);
+ 	search(lz - 1 + id, lz - 1 + uz, p, i0, i1);
  	return true;
  }
- // 求凸包上和向量 vec 叉积最大的点, 返回编号, 共线的多个切点返回任意一个
+ // 求凸包上和向量 vec 叉积最大的点编号, 共线返回任意一个
  int get_tan(point vec) {
- 	pair<LL, int> ret = get_tan(upper, vec);
- 	ret.second = (ret.second + (int)lower.size() - 1) % n;
+ 	pair<LD, int> ret = get_tan(upper, vec);
+ 	ret.second = (ret.second + lz - 1) % n;
  	ret = max(ret, get_tan(lower, vec));
  	return ret.second;
  }
  // 求凸包和直线 u,v 的交点, 如果无严格相交返回 false. 如果有则是和 (i,next(i)) 的交点, 两个点无序, 交在点上不确定返回前后两条线段其中之一
+ int search(point u, point v, int l, int r) {
+ 	int sl = sgn(det(v - u, a[l % n] - u));
+ 	for ( ; l + 1 < r; ) {
+ 		int mid = (l + r) / 2;
+ 		int smid = sgn(det(v - u, a[mid % n] - u));
+ 		if (smid == sl) l = mid;
+ 		else r = mid;
+ 	}
+ 	return l % n;
+ }
  bool get_inter(point u, point v, int &i0, int &i1) {
  	int p0 = get_tan(u - v), p1 = get_tan(v - u);
- 	if (sgn(det(v - u, a[p0] - u)) * sgn(det(v - u, a[p1] - u)) < 0) {
+ 	if (sgn(det(v-u, a[p0]-u))*sgn(det(v-u, a[p1]-u))<0) {
  		if (p0 > p1) swap(p0, p1);
  		i0 = search(u, v, p0, p1);
  		i1 = search(u, v, p1, p0 + n);
  		return true;
  	} else return false;
- }};
+ }
+ // 求凸包外一点到凸包的最短距离, 如凸包内返回 0; 结果产生浮点
+ LD s_near(int l, int r, point p) {
+ 	if (l == r) return dis (p, a[l % n]);
+ 	int sl = sgn(dot(a[l%n]-p, a[(l+1)%n]-a[l%n]));
+ 	for ( ; l + 1 < r; ) {
+		int m = (l + r) / 2;
+		int sm = sgn(dot(a[m%n]-p,a[(m+1)%n]-a[m%n]));
+		if (sm == sl) l = m; else r = m;
+	}
+	return point_to_segment(p, {a[l % n], a[(l + 1) % n]});
+ }
+ LD get_dis(point p) {
+ 	if (contain(p)) return 0;
+ 	LD dl = s_near(0, lz, p);
+ 	LD du = s_near(lz - 1, lz - 1 + uz, p);
+ 	return min(dl, du);
+ }
+};
