9ba7a0a411b1a0f268ad185ea46cb08e28ab82b9
fstqwq <fstqwq@foxmail.com>
2023-09-20 22:58:01 +0800

    update circle polygon intersect

--- src/src/Geometry/多边形和圆的交.cpp
+++ src/src/Geometry/多边形和圆的交.cpp
@@ -1,21 +1,13 @@
-LD angle (cp a, cp b) {
-	return 2 * asin(dis(a.unit(), b.unit()) / 2); }
-LD area(cp a, cp b, LD r) { // 2 * area
-	LD da = dot(a, a);
-	LD db = dot(b, b);
-	LD rr = r * r;
-	int sa = sgn(da - rr);
-	int sb = sgn(db - rr);
-	int sc = sgn(point_to_segment({0, 0}, {a, b}) - r);
-	if (sa <= 0 && sb <= 0) return det(a, b);
-	if (sc >= 0) return angle(a, b) * rr;
-	auto [u, v] = line_circle_inter({a, b}, {{0, 0}, r});
-	if (sa > 0 && sb > 0)
-		return det(u, v) + rr * (angle(a, u) + angle(v, b));
-	if (sa > 0)
-		return det(u, b) + angle(a, u) * rr;
-	else
-		return det(a, v) + angle(v, b) * rr; }
+LD angle (cp u, cp v) {
+	return 2 * asin(dis(u.unit(), v.unit()) / 2); }
+LD area(cp s, cp t, LD r) { // 2 * area
+	LD theta = angle(s, t); cp Z{0, 0};
+	LD dis = point_to_segment(Z, {s, t});
+	if (dis >= r) return theta * r * r;
+	auto [u, v] = line_circle_inter({s, t}, {Z, r});
+	point lo = sgn(det(s, u)) >= 0 ? u : s;
+	point hi = sgn(det(v, t)) >= 0 ? v : t;
+	return det(lo, hi) + (theta - angle(lo, hi)) * r * r; }
 LD solve(vector<point> &p, cc c) {
 	LD ret = 0;
 	for (int i = 0; i < (int) p.size (); ++i) {
