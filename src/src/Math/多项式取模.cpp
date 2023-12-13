9a86f02a66c8b10e7c64d6bbfb0cdead89f926db
AntiLeaf <antileaf@163.com>
2023-12-13 23:07:02 +0800

    更新 fft ntt mtt 多项式运算

---
+++ src/src/Math/多项式取模.cpp
@@ -0,0 +1,31 @@
+poly poly_auto_mul(poly a, poly b) { // 自动判断长度的乘法
+	int res_len = (int)a.size() + (int)b.size() - 1;
+	int ntt_n = 1; while (ntt_n < res_len) ntt_n *= 2;
+	a.resize(ntt_n); b.resize(ntt_n);
+	ntt(a.data(), ntt_n, 1); ntt(b.data(), ntt_n, 1);
+	for (int i = 0; i < ntt_n; i++)
+		a[i] = (ll)a[i] * b[i] % p;
+	ntt(a.data(), ntt_n, -1); a.resize(res_len); return a; }
+// 多项式除法，a 和 b 长度可以任意
+// 商的长度是 n - m + 1，余数的长度是 m - 1
+poly poly_div(const poly& a, const poly& b) {
+	int n = (int)a.size(), m = (int)b.size();
+	if (n < m) return {};
+	int ntt_n = 1; while (ntt_n < n - m + 1) ntt_n *= 2;
+	poly f(ntt_n), g(ntt_n);
+	for (int i = 0; i < n - m + 1; i++) f[i] = a[n - i - 1];
+	for (int i = 0; i < m && i < n - m + 1; i++)
+		g[i] = b[m - i - 1];
+	auto g_inv = poly_inv(g);
+	fill(g_inv.begin() + n - m + 1, g_inv.end(), 0);
+	auto c = poly_mul(f, g_inv); c.resize(n - m + 1);
+	reverse(c.begin(), c.end()); return c; }
+// 多项式取模，a 和 b 长度可以任意，返回 (余数，商)
+pair<poly, poly> poly_mod(const poly& a, const poly& b) {
+	int n = (int)a.size(), m = (int)b.size();
+	if (n < m) return {a, {}};
+	auto d = poly_div(a, b); auto c = poly_auto_mul(b, d);
+	poly r(m - 1);
+	for (int i = 0; i < m - 1; i++)
+		r[i] = (a[i] - c[i] + p) % p;
+	return {r, d}; }
--- src/src/Math/多项式运算.cpp
+++ src/src/Math/多项式运算.cpp
@@ -1,172 +1,51 @@
 using poly = vector<int>;
-poly omega[25]; // 单位根
-
-// n 是 DFT 的最大长度，例如如果最多有两个长为 m 的多项式相乘，
-// 或者求逆的长度为 m，那么 n 需要 >= 2m
-void ntt_init(int n) { // n = 2^k
-	for (int k = 2, d = 0; k <= n; k *= 2, d++) {
-		omega[d].resize(k + 1);
-
-		int wn = qpow(3, (p - 1) / k), tmp = 1;
-		for (int i = 0; i <= k; i++) {
-			omega[d][i] = tmp;
-			tmp = (long long)tmp * wn % p;
-		}
-	}
-
-	inv[0] = inv[1] = 1;
-	for (int i = 2; i < n; i++)
-		inv[i] = (ll)inv[p % i] * (p - p / i) % p;
-}
-
-// 传入的必须是 [0, p) 范围内，不能有负的
-// 否则把 d == 16 改成 d % 8 == 0 之类
-void ntt(int *c, int n, int t) {
-	static ull a[MAXN];
-	for (int i = 0; i < n; i++) a[i] = c[i];
-
-	for (int i = 1, j = 0; i < n - 1; i++) {
-		int k = n;
-		do
-			j ^= (k >>= 1);
-		while (j < k);
-
-		if (i < j)
-			swap(a[i], a[j]);
-	}
-
-	for (int k = 1, d = 0; k < n; k *= 2, d++) {
-		if (d == 16)
-			for (int i = 0; i < n; i++)
-				a[i] %= p;
-
-		for (int i = 0; i < n; i += k * 2)
-			for (int j = 0; j < k; j++) {
-				int w = omega[d][t > 0 ? j : k * 2 - j];
-				ull u = a[i + j], v = w * a[i + j + k] % p;
-				a[i + j] = u + v;
-				a[i + j + k] = u - v + p;
-			}
-		}
-		
-	if (t > 0) {
-		for (int i = 0; i < n; i++)
-			c[i] = a[i] % p;
-	}
-	else {
-		int inv = qpow(n, p - 2);
-		for (int i = 0; i < n; i++)
-			c[i] = a[i] * inv % p;
-	}
-}
-
-// u, v 长度要相同，返回长度是两倍
-poly poly_calc(const poly& u, const poly& v, function<int(int, int)> op) {
+poly poly_calc(const poly& u, const poly& v, // 长度要相同
+	function<int(int, int)> op) { // 返回长度是两倍
 	static int a[MAXN], b[MAXN], c[MAXN];
-
 	int n = (int)u.size();
-
 	memcpy(a, u.data(), sizeof(int) * n);
 	fill(a + n, a + n * 2, 0);
 	memcpy(b, v.data(), sizeof(int) * n);
 	fill(b + n, b + n * 2, 0);
-
-	ntt(a, n * 2, 1);
-	ntt(b, n * 2, 1);
-
-	for (int i = 0; i < n * 2; i++)
-		c[i] = op(a[i], b[i]);
-
-	ntt(c, n * 2, -1);
-
-	return poly(c, c + n * 2);
-}
-
-// 乘法，返回长度是两倍
-poly poly_mul(const poly& u, const poly& v) {
-	return poly_calc(u, v, [](int a, int b) { return (ll)a * b % p; });
-}
-
-// 求逆，返回长度不变
-poly poly_inv(const poly& a) {
+	ntt(a, n * 2, 1); ntt(b, n * 2, 1);
+	for (int i = 0; i < n * 2; i++) c[i] = op(a[i], b[i]);
+	ntt(c, n * 2, -1); return poly(c, c + n * 2); }
+poly poly_mul(const poly& u, const poly& v) { // 乘法
+	return poly_calc(u, v, [](int a, int b)
+		{ return (ll)a * b % p; }); } // 返回长度是两倍
+poly poly_inv(const poly& a) { // 求逆，返回长度不变
 	poly c{qpow(a[0], p - 2)}; // 常数项一般都是 1
-
 	for (int k = 2; k <= (int)a.size(); k *= 2) {
-		c.resize(k);
-
-		poly b(a.begin(), a.begin() + k);
+		c.resize(k); poly b(a.begin(), a.begin() + k);
 		c = poly_calc(b, c, [](int bi, int ci) {
-			return ((2 - (ll)bi * ci) % p + p) * ci % p;
-		});
-		memset(c.data() + k, 0, sizeof(int) * k);
-	}
-
-	c.resize(a.size());
-	return c;
-}
-
-// 开根，返回长度不变
-poly poly_sqrt(const poly& a) {
+			return ((2 - (ll)bi * ci) % p + p) * ci % p; });
+		memset(c.data() + k, 0, sizeof(int) * k); }
+	c.resize(a.size()); return c; }
+poly poly_sqrt(const poly& a) { // 开根，返回长度不变
 	poly c{1}; // 常数项不是 1 的话要写二次剩余
-
 	for (int k = 2; k <= (int)a.size(); k *= 2) {
-		c.resize(k);
-
-		poly b(a.begin(), a.begin() + k);
+		c.resize(k); poly b(a.begin(), a.begin() + k);
 		b = poly_mul(b, poly_inv(c));
-
-		for (int i = 0; i < k; i++)
-			c[i] = (ll)(c[i] + b[i]) * inv_2 % p; // inv_2 是 2 的逆元
-	}
-
-	c.resize(a.size());
-	return c;
-}
-
-// 求导
-poly poly_derivative(const poly& a) {
-	poly c(a.size());
-	for (int i = 1; i < (int)a.size(); i++)
-		c[i - 1] = (ll)a[i] * i % p;
-	return c;
-}
-
-// 不定积分，最好预处理逆元
-poly poly_integrate(const poly& a) {
-	poly c(a.size());
-	for (int i = 1; i < (int)a.size(); i++)
-		c[i] = (ll)a[i - 1] * inv[i] % p;
-	return c;
-}
-
-// ln，常数项不能为 0，返回长度不变
-poly poly_ln(const poly& a) {
+		for (int i = 0; i < k; i++) // inv_2 是 2 的逆元
+			c[i] = (ll)(c[i] + b[i]) * inv_2 % p; }
+	c.resize(a.size()); return c; }
+poly poly_derivative(const poly& a) { poly c(a.size());
+	for (int i = 1; i < (int)a.size(); i++) // 求导
+		c[i - 1] = (ll)a[i] * i % p; return c; }
+poly poly_integrate(const poly& a) { poly c(a.size());
+	for (int i = 1; i < (int)a.size(); i++) // 不定积分
+		c[i] = (ll)a[i - 1] * inv[i] % p; return c; }
+poly poly_ln(const poly& a) { // ln,常数项非0,返回长度不变
 	auto c = poly_mul(poly_derivative(a), poly_inv(a));
-	c.resize(a.size());
-	return poly_integrate(c);
-}
-
+	c.resize(a.size()); return poly_integrate(c); }
 // exp，常数项必须是 0，返回长度不变
 // 常数很大并且总代码很长, 一般可以改用分治 FFT
 // 依据: 设 $G(x) = \exp F(x)$, 则 $g_i = \sum_{k=1}^{i-1} f_{i-k} * k * g_k$
-poly poly_exp(const poly& a) {
-	poly c{1};
-
+poly poly_exp(const poly& a) { poly c{1};
 	for (int k = 2; k <= (int)a.size(); k *= 2) {
-		c.resize(k);
-		
-		auto b = poly_ln(c);
-		for (int i = 0; i < k; i++) {
-			b[i] = a[i] - b[i];
-			if (b[i] < 0)
-				b[i] += p;
-		}
-		(++b[0]) %= p;
-
-		c = poly_mul(b, c);
-		memset(c.data() + k, 0, sizeof(int) * k);
-	}
-
-	c.resize(a.size());
-	return c;
-}
+		c.resize(k); auto b = poly_ln(c);
+		for (int i = 0; i < k; i++)
+			b[i] = (a[i] - b[i] + p) % p;
+		(++b[0]) %= p; c = poly_mul(b, c);
+		memset(c.data() + k, 0, sizeof(int) * k); }
+	c.resize(a.size()); return c; }
