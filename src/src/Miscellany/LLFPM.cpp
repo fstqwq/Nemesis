1277e0e39aacc576ead9de469300e76bd23e6038
fstqwq <fstqwq@foxmail.com>
2023-12-29 18:51:40 +0800

    modify ulimit; add barrett

--- src/src/Miscellany/LLFPM.cpp
+++ src/src/Miscellany/LLFPM.cpp
@@ -7,4 +7,12 @@ ULL modmul(ULL a, ULL b, LL M) {
 	ULL c = (long double)a * b / M;
 	LL ret = LL(x * y - c * M) % M; // must be signed
 	return ret < 0 ? ret + M : ret;}
-// use int128 instead if c > 63 bit
+// use int128 instead if M > 63 bit
+struct DIV { 
+	ULL p, ip;
+	void init (ULL _p) { p = _p; ip = -1llu / p; }
+	int mod (ULL x) { // x < 2 ^ 64
+		ULL q = ULL(((u128)iP * x) >> 64);
+		ULL r = x - q * p;
+		return int(r >= p ? r - p : r);
+} }; // speedup only when mod is not const
