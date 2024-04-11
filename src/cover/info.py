575cfc51e480b0b92ce77a33b3f533b46b60b6a2
fstqwq <fstqwq@foxmail.com>
2024-04-11 16:14:03 +0800

    add joker zjj

--- src/cover/info.py
+++ src/cover/info.py
@@ -1,5 +1,14 @@
-import sys, os
+import sys, os, datetime
 
-f = open("../.git/refs/heads/master", "r")
+f = open("../.git/logs/refs/heads/master", "r")
 
-print(f.read()[:7])
+lines = list(f.readlines())
+
+while len(lines[-1]) <= 10:
+    lines.pop()
+line = lines[-1].split()
+
+commit = line[1]
+stamp = line[4]
+
+print(f"{datetime.datetime.fromtimestamp(int(stamp)).strftime('%h %d, %Y')} \\texttt{{({commit[:6]})}}")
--- src/src/String/hash.cpp
+++ src/src/String/hash.cpp
@@ -1,34 +1,8 @@
-const int H = 2; // 换 base 验证 base ^ {1...n} != 1
-const int P[] = {318255569, 19873577, 18611951},
-          Q[] = {1010451419, 1011111133, 1033111117};
-int pw[H][N];
-struct hashInit { hashInit () {
-  for (int h = 0; h < H; h++) {
-    pw[h][0] = 1;
-    for (int i = 1; i < N; i++)
-      pw[h][i] = (LL)pw[h][i - 1] * P[h] % Q[h];
-  } } } __init_hash;
-struct Hash {
-int v[H], len;
-Hash () {memset(v, 0, sizeof v); len = 0;}
-Hash (int x) {len = 1;for (int h = 0; h < H; h++) v[h] = x;}
-};Hash operator + (const Hash &a, const int &b) {
-  Hash ret; ret.len = a.len + 1;
-  for (int h = 0; h < H; h++) 
-    ret.v[h] = ((LL)a.v[h] * P[h] + b) % Q[h];
-  return ret; }
-Hash operator - (const Hash &a, const Hash &b) {
-  Hash ret; ret.len = a.len - b.len;
-  for (int h = 0; h < H; h++) {
-    ret.v[h] = (a.v[h] - (LL)pw[h][ret.len]*b.v[h]) % Q[h];
-    if (ret.v[h] < 0) ret.v[h] += Q[h];
-  } return ret; }
-bool operator == (const Hash &a, const Hash &b) {
-  for (int h = 0; h < H; h++) 
-    if (a.v[h] != b.v[h]) return false;
-  return a.len == b.len; }
-Hash operator + (const Hash &a, const Hash &b) {
-  Hash ret; ret.len = a.len + b.len;
-  for (int h = 0; h < H; h++) 
-    ret.v[h] = ((LL)a.v[h] * pw[h][b.len] + b.v[h]) % Q[h];
-  return ret; }
+static constexpr u128 inv = []() {
+	u128 ret = P;
+	for (int i = 0; i < 6; i++) ret *= 2 - ret * P;
+	return ret; }();
+constexpr u128 chk = u128(-1) / P;
+bool check(i128 a, i128 b) {
+	if (a < b) swap(a, b);
+	return (a - b) * inv <= chk; }
