a10231e6e188a5f75a1859f1eb39843467156eea
fstqwq <fstqwq@foxmail.com>
2024-04-12 13:59:45 +0800

    use a correct git info parse

--- src/cover/info.py
+++ src/cover/info.py
@@ -1,14 +1,14 @@
-import sys, os, datetime
+import sys, os, datetime, subprocess
 
-f = open("../.git/logs/refs/heads/master", "r")
+result = subprocess.run(["git", "log", "-n", "1"], stdout=subprocess.PIPE)
 
-lines = list(f.readlines())
+lines = result.stdout.splitlines()
 
-while len(lines[-1]) <= 10:
-    lines.pop()
-line = lines[-1].split()
+date_str = lines[2].decode()
+date_format = "Date:   %a %b %d %H:%M:%S %Y %z"
+stamp = datetime.datetime.strptime(date_str, date_format).timestamp()
 
-commit = line[1]
-stamp = line[4]
+commit = lines[0].decode().split()[1]
+info = lines[4].decode().strip()
 
-print(f"{datetime.datetime.fromtimestamp(int(stamp)).strftime('%h %d, %Y')} {{({commit[:6]}: {' '.join(line[7:])})}}")
+print(f"{datetime.datetime.fromtimestamp(int(stamp)).strftime('%h %d, %Y')} ({commit[:6]}: {info})")
