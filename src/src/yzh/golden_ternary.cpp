f6ec08529f6b4d0d3b10e0164371e20e3a7b0630
fstqwq <fstqwq@foxmail.com>
2024-06-26 11:42:24 +0800

    fix tenary search

--- src/src/yzh/golden_ternary.cpp
+++ src/src/yzh/golden_ternary.cpp
@@ -1,7 +1,7 @@
 constexpr LD R = (sqrt(5) - 1) / 2;
 auto split = [](LD l, LD r) { return l + (r - l) * R; };
 LD solve(LD a, LD c, auto f) {
-	LD b = split(a, b), bv = f(b);
+	LD b = split(a, c), bv = f(b);
 	for (int _ = T; _; _--) {
 		LD x = split(a, b), xv = f(x);
 		if (xv > bv) c = b, b = x, bv = xv;
