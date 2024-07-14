9880a2649fc2bc2d426d7d6289ecea2ed8a34e24
weakpeople <zhangjianjunab@126.com>
2024-07-14 11:48:30 +0800

    change PAM.cpp

--- src/src/String/PAM.cpp
+++ src/src/String/PAM.cpp
@@ -1,9 +1,11 @@
 int len[N], par[N], go[N][26], last, pam_cnt; char s[N];
-void extend(int n) { int p = last, c = s[n] - 'a';
+void init(){ par[0] = pam_cnt = 1; len[1] = -1; }
+int getfail(int n,int p){
 	while (s[n - len[p] - 1] != s[n]) p = par[p];
+	return p;}
+void extend(int n) { int p = getfail(n, last), c = s[n] - 'a';
 	if (!go[p][c]) { int q = ++pam_cnt, now = p;
 		len[q] = len[p] + 2;
-		do p = par[p]; while (s[n - len[p] - 1] != s[n]);
+		p = getfail(n, par[p]);
 		par[q] = go[p][c]; last = go[now][c] = q;
 	} else last = go[p][c]; }
-int main() { par[0] = pam_cnt = 1; len[1] = -1; }
