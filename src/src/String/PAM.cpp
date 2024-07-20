0b866561cbc4b651b0319d2c54f5de207dfceb8a
weakpeople <zhangjianjunab@126.com>
2024-07-20 22:21:01 +0800

    change SAM's val->mxl , means maxlen ; change PAM, delete zjj's PAM and biderection PAM

--- src/src/String/PAM.cpp
+++ src/src/String/PAM.cpp
@@ -1,12 +1,22 @@
-int len[N], par[N], go[N][26], last, pam_cnt; char s[N];
-void init(){ par[0] = pam_cnt = 1; len[1] = -1; }
+int len[N], fail[N], go[N][26], last, pam_cnt; char s[N];
+int dif[N], slink[N], g[N]; // 最小回文划分相关的 dp
+void init(){ fail[0] = pam_cnt = 1; len[1] = -1; }
 int getfail(int n, int p){
-	while (s[n - len[p] - 1] != s[n]) p = par[p];
+	while (s[n - len[p] - 1] != s[n]) p = fail[p];
 	return p;}
-void extend(int n) {
+void extend(int n) {// 1 - base
 	int p = getfail(n, last), c = s[n] - 'a';
 	if (!go[p][c]) { int q = ++pam_cnt, now = p;
 		len[q] = len[p] + 2;
-		p = getfail(n, par[p]);
-		par[q] = go[p][c]; last = go[now][c] = q;
-	} else last = go[p][c]; }
+		p = getfail(n, fail[p]);
+		fail[q] = go[p][c]; last = go[now][c] = q;
+		dif[q] = len[q] - len[fail[q]];
+		slink[q] = dif[q] == dif[fail[q]] ? 
+		slink[fail[q]] : fail[q];} else last = go[p][c];}
+void solve(){
+	for(int x = last; x > 1; x = slink[x]){
+		g[x] = dp[i - len[slink[x]] - dif[x]];
+		if(dif[x] == dif[fail[x]]) g[x] <- g[fail[x]];
+		dp[i] <- g[x]; } }
+//双端插入 : 前缀后缀分别写一个last和getfail,回文所以只需要维护回文后缀,当last=串长的时候两个last要等于整个串
+//广义PAM : 
--- src/src/String/SAM.cpp
+++ src/src/String/SAM.cpp
@@ -1,10 +1,10 @@
-int last, val[MAXN], par[MAXN], go[MAXN][26], sam_cnt;
+int last, mxl[MAXN], par[MAXN], go[MAXN][26], sam_cnt;
 void extend(int c) { // 结点数要开成串长的两倍
-	int p = last, np = ++sam_cnt; val[np] = val[p] + 1;
+	int p = last, np = ++sam_cnt; mxl[np] = mxl[p] + 1;
 	while (p && !go[p][c]) { go[p][c] = np; p = par[p]; }
 	if (!p) par[np] = 1; else { int q = go[p][c];
-		if (val[q] == val[p] + 1) par[np] = q;
-		else { int nq = ++sam_cnt; val[nq] = val[p] + 1;
+		if (mxl[q] == mxl[p] + 1) par[np] = q;
+		else { int nq = ++sam_cnt; mxl[nq] = mxl[p] + 1;
 			memcpy(go[nq], go[q], sizeof(go[q]));
 			par[nq] = par[q]; par[np] = par[q] = nq;
 			while (p && go[p][c] == q) { go[p][c] = nq;
@@ -12,6 +12,6 @@ void extend(int c) { // 结点数要开成串长的两倍
 void init() { last = sam_cnt = 1; }
 int c[MAXN], q[MAXN]; 
 void solve() { // 跑完得到的q是一个合法的拓扑序
-	for (int i = 1; i <= sam_cnt; i++) c[val[i] + 1]++;
+	for (int i = 1; i <= sam_cnt; i++) c[mxl[i] + 1]++;
 	for (int i = 1; i <= n; i++) c[i] += c[i - 1];
-	for (int i = 1; i <= sam_cnt; i++) q[++c[val[i]]] = i;}
+	for (int i = 1; i <= sam_cnt; i++) q[++c[mxl[i]]] = i;}
