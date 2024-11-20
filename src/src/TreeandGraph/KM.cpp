fde4d1017f13db6aee0b6839bcc7d0418c96bb11
fstqwq <fstqwq@foxmail.com>
2024-11-20 21:37:22 +0800

    add rainbow-km

--- src/src/TreeandGraph/KM.cpp
+++ src/src/TreeandGraph/KM.cpp
@@ -3,19 +3,19 @@ vector<int> KM(int n, int m) {
 	vector<LL> l(n + 1), r(m + 1);
 	vector<int> p(m + 1), ans(n + 1);
 	for (int i = 1; i <= n; ++i) {
-		vector<LL> d(m + 1, 1e18);
-		vector<int> pre(m + 1), done(m + 1);
+		vector<LL> d(m + 1, INF);
+		vector<int> pre(m + 1), ok(m + 1);
 		int x, v, u = 0;
 		for (p[0] = i; x = p[u]; u = v) {
-			done[u] = 1;
-			LL min = 1e18;
-			for (int j = 1; j <= m; ++j) if (!done[j]) {
+			ok[u] = 1;
+			LL min = INF;
+			for (int j = 1; j <= m; ++j) if (!ok[j]) {
 				auto w = e[x][j] - l[x] - r[j];
 				if (w < d[j]) d[j] = w, pre[j] = u;
 				if (d[j] < min) min = d[j], v = j;
 			}
 			for (int j = 0; j <= m; ++j) {
-				if (done[j]) l[p[j]] += min, r[j] -= min;
+				if (ok[j]) l[p[j]] += min, r[j] -= min;
 				else d[j] -= min;
 			}
 		}
