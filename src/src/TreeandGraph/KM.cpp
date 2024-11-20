e81a6d68d76c063bf1dea169e9290f0f866715d3
antileaf <antileaf@163.com>
2024-11-20 21:08:53 +0800

    qwq

--- src/src/TreeandGraph/KM.cpp
+++ src/src/TreeandGraph/KM.cpp
@@ -1,43 +1,30 @@
-struct KM {
-int n, nl, nr;
-T g[N][N], lx[N], ly[N], slack[N];
-int mx[N], my[N], visx[N], visy[N], pre[N], q[N], ql, qr;
-int check(int i) {
-	visy[i] = 1;
-	if (~my[i]) {
-		q[qr++] = my[i], visx[my[i]] = 1;
-		return 0; }
-	while (~i) my[i] = pre[i], swap(i, mx[pre[i]]);
-	return 1; }
-void bfs(int s) {
-	ql = 0, qr = 1;
-	q[ql] = s, visx[s] = 1;
-	for (T d; ; ) {
-		while (ql < qr)
-		for (int v = 0, u = q[ql++]; v < n; v++)
-		if (!visy[v] && slack[v] >= (d=lx[u]+ly[v]-g[u][v])) {
-			pre[v] = u;
-			if (d) slack[v] = d; else if (check(v)) return;
-		} d = INF;
-		for (int i = 0; i < n; i++)
-			if (!visy[i]) d = min(d, slack[i]);
-		for (int i = 0; i < n; i++) {
-			if (visy[i]) ly[i] += d; else slack[i] -= d;
-			if (visx[i]) lx[i] -= d; }
-		for (int i = 0; i < n; i++)
-			if (!visy[i] && !slack[i] && check(i)) return;
-	} }
-void solve() {
-	n = max(nl, nr); // always compute a full matching
-	fill(pre, pre + n, -1); 
-	fill(mx, mx + n, -1); fill(my, my + n, -1);
-	fill(ly, ly + n, 0);
-	for (int i = 0; i < n; i++)
-		lx[i] = *max_element(g[i], g[i] + n);
-	for (int i = 0; i < n; i++) {
-		fill(slack, slack + n, INF);
-		fill(visx, visx + n, 0); fill(visy, visy + n, 0);
-		bfs(i); }
-	for (int i = 0; i < n; i++)
-		if (g[i][mx[i]] == 0) mx[i] = -1;
-} } km;
+LL e[N][M];
+vector<int> KM(int n, int m) {
+	vector<LL> l(n + 1), r(m + 1);
+	vector<int> p(m + 1), ans(n + 1);
+	for (int i = 1; i <= n; ++i) {
+		vector<LL> d(m + 1, 1e18);
+		vector<int> pre(m + 1), done(m + 1);
+		int x, v, u = 0;
+		for (p[0] = i; x = p[u]; u = v) {
+			done[u] = 1;
+			LL min = 1e18;
+			for (int j = 1; j <= m; ++j) if (!done[j]) {
+				auto w = e[x][j] - l[x] - r[j];
+				if (w < d[j]) d[j] = w, pre[j] = u;
+				if (d[j] < min) min = d[j], v = j;
+			}
+			for (int j = 0; j <= m; ++j) {
+				if (done[j]) l[p[j]] += min, r[j] -= min;
+				else d[j] -= min;
+			}
+		}
+		for (int v; u; u = v) {
+			v = pre[u], p[u] = p[v];
+		}
+	}
+	for (int j = 1; j <= m; ++j) {
+		ans[p[j]] = j;
+	}
+	return ans;
+}
